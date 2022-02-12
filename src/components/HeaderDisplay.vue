0fed5404cb80b83e19d1c43dd2dfded30b41f691
DIALL-Dev <96806921+DIALL-Dev@users.noreply.github.com>
2022-02-12 11:34:26 +0900

    first commit

---
+++ src/components/HeaderDisplay.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    <h2>Weather Application</h2>
+    <p>{{ username }}님 반갑습니다.</p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderDisplay",
+};
+</script>
+
+<style scoped>
+.header {
+  background: linear-gradient(15deg, #8e2de2, #4a00e0);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em;
+}
+.header h2 {
+  color: #ffffff;
+}
+.header p {
+  font-size: 12px;
+  color: #ffffff;
+}
+</style>
